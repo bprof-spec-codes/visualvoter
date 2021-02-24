20aa5df16fe0ec638a45fa50c97ed61d7429d56b
Bence Papp <bencepapp1101@gmail.com>
2021-02-24 17:51:40 +0100

    header effects added

--- votoe-frontend/src/components/Header.js
+++ votoe-frontend/src/components/Header.js
@@ -1,31 +1,33 @@
 import React from "react";
 import "./Header.css";
-import HomeIcon from "@material-ui/icons/Home";
+import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
 import HowToVoteIcon from "@material-ui/icons/HowToVote";
-import AddBoxIcon from "@material-ui/icons/AddBox";
-import logo from '../assets/img/wv4y1e5r.png';
+import HowToVoteOutlinedIcon from "@material-ui/icons/HowToVoteOutlined";
+import logo from "../assets/img/wv4y1e5r.png";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header_left">
-        <img src={logo} alt="logo" />
-      </div>
+      <div className="header_container">
+        <div className="header_left">
+          <img src={logo} alt="logo" />
+        </div>
 
-      <div className="header_middle">
+        <div className="header_middle">
           <div className="header_option">
-        <HomeIcon />
+            <HomeOutlinedIcon fontSize="large" />
+          </div>
+          <div className="header_option">
+            <HowToVoteOutlinedIcon fontSize="large" />
+          </div>
+          <div className="header_option">
+            <HowToVoteOutlinedIcon fontSize="large" />
+          </div>
         </div>
-        <div className="header_option">
-        <HowToVoteIcon />
-        </div>
-        <div className="header_option">
-        <AddBoxIcon />
-        </div>
-      </div>
 
-      <div className="header_right">
-        <p>Username</p>
+        <div className="header_right">
+          <p>Username</p>
+        </div>
       </div>
     </div>
   );
