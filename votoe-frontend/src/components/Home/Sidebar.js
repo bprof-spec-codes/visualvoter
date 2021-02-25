79fdf75ae410cc1b1c72bc87fb665f1fdfd00fd9
Bence Papp <bencepapp1101@gmail.com>
2021-02-25 09:48:52 +0100

    sidebar updated

--- votoe-frontend/src/components/Home/Sidebar.js
+++ votoe-frontend/src/components/Home/Sidebar.js
@@ -1,8 +1,13 @@
-import React from "react";
-import Modal from 'react-modal';
+import { SportsRugbySharp } from "@material-ui/icons";
+import CloseOutlinedIcon from "@material-ui/icons/CloseOutlined";
+import React, { useState } from "react";
+import Modal from "react-modal";
 import "./Sidebar.css";
 
+Modal.setAppElement("#root");
 function Sidebar() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <div className="sidebar">
       <div className="sidebar_container">
@@ -11,7 +16,7 @@ function Sidebar() {
           <p>Profile</p>
         </div>
 
-        <div className="sidebar_option">
+        <div className="sidebar_option" onClick={() => setModalOpen(true)}>
           <p>Invites</p>
           <label>0</label>
         </div>
@@ -19,6 +24,46 @@ function Sidebar() {
         <div className="sidebar_option">
           <p>News</p>
         </div>
+      </div>
+
+      <div className="sidebar_modal">
+        <Modal
+          isOpen={modalOpen}
+          onRequestClose={() => setModalOpen(false)}
+          style={{
+            overlay: {
+              backgroundColor: "rgba(1,1,1,0.6)",
+              position: "fixed",
+              marign: 0,
+            },
+            content: {
+              width: 500,
+              height: "auto",
+              borderRadius: 20,
+              position: "absolute",
+              top: "10%",
+              left: "36%",
+
+              display: "flex",
+              justifyContent: "space-between",
+              padding: 30,
+            },
+          }}
+        >
+          <div
+            className="modal_top"
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width:'100%'
+            }}
+          >
+            <h1>Invites</h1>
+            <div className="modal_close" onClick={() => setModalOpen(false)}>
+              <CloseOutlinedIcon fontSize="large" />
+            </div>
+          </div>
+        </Modal>
       </div>
     </div>
   );
