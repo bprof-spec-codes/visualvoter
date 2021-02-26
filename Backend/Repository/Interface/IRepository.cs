44b4380213cbbac3d98ff7d79a6f17633287f6d7
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-02-26 12:22:50 +0100

    user repository interface added

---
+++ Backend/Repository/Interface/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public interface IRepository<TReturnType, TKeyType>
+    {
+        TReturnType GetOne(TKeyType key);
+        IQueryable<TReturnType> GetAll();
+    }
+}
---
+++ Backend/Repository/Interface/IUsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    interface IUsersRepository : IRepository<Users,int>
+    {
+        void CreateUser(Users user);
+        new Users GetOne(int UserId);
+        new IQueryable<Users> GetAll();
+    }
+}
