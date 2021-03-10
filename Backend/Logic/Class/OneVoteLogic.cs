9f07b43c29820db78ed69806803c894928effdfe
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-03-10 15:29:45 +0100

    logic for checking if a vote is eligible added

--- Backend/Logic/Class/OneVoteLogic.cs
+++ Backend/Logic/Class/OneVoteLogic.cs
@@ -10,14 +10,20 @@ namespace Logic
     public class OneVoteLogic : IOneVoteLogic
     {
         public IOneVoteRepository oneVoteRepo;
+        public IAllVotesLogic allVotesLogic;
+        public IVotingRightLogic vrLogic;
+        public IUsersLogic usersLogic;
 
         public OneVoteLogic(string dbPassword)
         {
             this.oneVoteRepo = new OneVoteRepository(dbPassword);
+            this.allVotesLogic = new AllVotesLogic(dbPassword);
+            this.vrLogic = new VotingRightLogic(dbPassword);
+            this.usersLogic = new UsersLogic(dbPassword);
         }
         public bool CreateOneVote(OneVote vote)
         {
-            //this.oneVoteRepo.Add(vote);
+            if (!CanVote(vote)) return false;
             try
             {
                 this.oneVoteRepo.Add(vote);
@@ -65,6 +71,50 @@ namespace Logic
             {
                 return false;
             }
+        }
+
+        private bool CanVote(OneVote vote)
+        {
+            var userTypesWhoCanVote = this.vrLogic.GetOne(vote.VoteID);
+            var userTypeID = this.usersLogic.GetOneUser(vote.UserID).UserTypeID;
+            foreach(var votingRightsRow in userTypesWhoCanVote)
+            {
+                if(votingRightsRow.UserTypeID == userTypeID) // true = the user can vote
+                {
+                    //check if voted before
+                    if (GetUsersVoteHistory(vote.UserID, vote.VoteID))
+                    {
+                        var voteRow = this.allVotesLogic.GetOneVote(vote.VoteID);
+                        switch (vote.Choice)
+                        {
+                            case 0:
+                                voteRow.YesVotes += 1;
+                                this.allVotesLogic.UpdateVote(voteRow.VoteID, voteRow);
+                                break;
+                            case 1:
+                                voteRow.NoVotes += 1;
+                                this.allVotesLogic.UpdateVote(voteRow.VoteID, voteRow);
+                                break;
+                            case 2:
+                                voteRow.AbsentionVotes += 1;
+                                this.allVotesLogic.UpdateVote(voteRow.VoteID, voteRow);
+                                break;
+                            default: return false;
+                        }
+                        return true;
+                    }
+                    else return false;
+                }
+            }
+            return false;
+        }
+
+        private bool GetUsersVoteHistory(int userID, int voteID)
+        {
+            var query = from x in oneVoteRepo.GetAll()
+                        where x.UserID == userID && x.VoteID == voteID
+                        select x;
+            return query == null;
         }
     }
 }
