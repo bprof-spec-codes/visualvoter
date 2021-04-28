3d25e072bf0f81ebdcca6b873498c9e7e0408029
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-04-28 11:12:56 +0200

    roleswitch model added

---
+++ Backend/Models/RoleSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class RoleSwitch
+    {
+        [Key]
+        public int OneRoleSwitchID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string RoleName { get; set; }
+    }
+}
