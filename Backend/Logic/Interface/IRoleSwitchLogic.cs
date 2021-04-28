11a32e69e2b708b2f82cb500005845e056c4b508
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-04-28 11:25:40 +0200

    interface now has the methods

--- Backend/Logic/Interface/IRoleSwitchLogic.cs
+++ Backend/Logic/Interface/IRoleSwitchLogic.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Logic
 {
     public interface IRoleSwitchLogic
     {
+        bool Delete(int id);
+        IQueryable<RoleSwitch> GetAllRoleSwitchRequests();
+        RoleSwitch GetOne(int id);
+        bool AlreadyAppliedForRole(string userName);
+        bool RequestNewRole(string roleName, string userName);
     }
 }
